5e2723674bf90c81ef60d851941b755ecf4b1ce2
Suprateek Dhar <118747426+sdcardbro123@users.noreply.github.com>
2023-12-21 16:06:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as npy
 from sklearn.tree import DecisionTreeClassifier
 
 #now the csv file is imported
-M = pd.read_csv(r"music.csv")
+M = pd.read_txr(r"music.txt")
 M #rub this if you dont want to inspect this
 #splitting this into sections
+X= M['
